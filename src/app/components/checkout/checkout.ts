df88cb8d9d14b7138edf513db6d861ff1ced81be
nika.kachashvili <Nika.KACHASHVILI@akkodis.com>
2025-09-01 11:59:35 +0200

    - shop form service added to calculate exp. month and year dynamically

--- src/app/components/checkout/checkout.ts
+++ src/app/components/checkout/checkout.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ShopFormService } from '../../services/shop-form-service';
+
 
 @Component({
   selector: 'app-checkout',
@@ -13,10 +15,11 @@ export class Checkout {
   checkoutFormGroup!: FormGroup;
   totalPrice: number = 0.00;
   totalQuantity: number = 0;
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
 
-  constructor(private formBuilder: FormBuilder) {
-
-  }
+  constructor(private formBuilder: FormBuilder,
+    private shopFormService: ShopFormService) { }
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group(
@@ -59,21 +62,40 @@ export class Checkout {
         ),
       }
     );
+
+    // populate credit card months and years
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log("startMonth: " + startMonth);
+
+    this.shopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved months data: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    )
+
+    this.shopFormService.getCreditCardYears().subscribe(
+      data => {
+        console.log("Retrieved years data: " + JSON.stringify(data));
+        this.creditCardYears = data;
+      }
+    )
+
   }
 
   onSubmit() {
     console.log(JSON.stringify(this.checkoutFormGroup.value, null, 2));
   }
 
-onChange() {
-  if (this.checkoutFormGroup.get('sameAsShipping')?.value) {
-    const shipping = this.checkoutFormGroup.get('shippingAddress')?.value;
-    this.checkoutFormGroup.get('billingAddress')?.setValue(shipping);
-    console.log(this.checkoutFormGroup.get('customer')?.value);
-  } else {
-    this.checkoutFormGroup.get('billingAddress')?.reset();
+  onChange() {
+    if (this.checkoutFormGroup.get('sameAsShipping')?.value) {
+      const shipping = this.checkoutFormGroup.get('shippingAddress')?.value;
+      this.checkoutFormGroup.get('billingAddress')?.setValue(shipping);
+      console.log(this.checkoutFormGroup.get('customer')?.value);
+    } else {
+      this.checkoutFormGroup.get('billingAddress')?.reset();
+    }
   }
-}
 
 
 }
