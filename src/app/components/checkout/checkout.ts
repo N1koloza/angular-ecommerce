1e9219ddaba0d76e34470a57e100096b1e1ec7a8
nika.kachashvili <Nika.KACHASHVILI@akkodis.com>
2025-09-08 16:53:19 +0200

    - add handleMonthesAndYears()

--- src/app/components/checkout/checkout.ts
+++ src/app/components/checkout/checkout.ts
@@ -97,5 +97,28 @@ export class Checkout {
     }
   }
 
+  handleMonthesAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
+    let startMonth: number;
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+
+    } else {
+      startMonth = 1;
+    }
+
+    this.shopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+
+      }
+    )
+
+  }
+
 
 }
